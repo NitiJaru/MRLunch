7045bfc3a3ac95000bac888de8ecdf1b0c333346
Earn <earndarkness@perfenterprise.com>
2018-09-01 16:43:26 +0700

    Implement result poll
    Signed-off-by: Earn <earndarkness@perfenterprise.com>

--- MrLunch/src/MrLunchIonic/src/pages/home/home.ts
+++ MrLunch/src/MrLunchIonic/src/pages/home/home.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import { ManageshopPage } from '../manageshop/manageshop';
-import { CreatlistPage } from '../creatlist/creatlist';
 import { HttpClient } from '@angular/common/http';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/timeout';
+
+import { ManageshopPage } from '../manageshop/manageshop';
+import { CreatlistPage } from '../creatlist/creatlist';
+import { ResualPage } from '../resual/resual';
 
 @Component({
   selector: 'page-home',
@@ -12,7 +14,9 @@ import 'rxjs/add/operator/timeout';
 })
 export class HomePage {
 
+  selectmenuid: any;
   model: any;
+
   constructor(public navCtrl: NavController, private httpClient: HttpClient) {
   }
 
@@ -20,34 +24,43 @@ export class HomePage {
     this.httpClient.get("https://mrlunch.azurewebsites.net/api/Polls/GetRestaurantPolls")
       .subscribe((data: any) => {
         this.model = data[0];
-        console.log(JSON.stringify(this.model));
       }, error => {
         alert(error.message);
       });
   }
 
-  Gomanage() {
-    this.navCtrl.push(ManageshopPage);
-  }
+  Gomanage() { this.navCtrl.push(ManageshopPage); }
 
-  createpoll() {
-    this.navCtrl.push(CreatlistPage)
-  }
+  createpoll() { this.navCtrl.push(CreatlistPage) }
 
-  selectmenu(id: any) {
-    console.log(JSON.stringify(id));
-  }
+  selectmenu(id: any) { this.selectmenuid = id }
 
-  doRefresh(refresher){
-    setTimeout(() => {
-      
-      this.httpClient.get("https://mrlunch.azurewebsites.net/api/Polls/GetRestaurantPolls")
+  submit() {
+    if (this.selectmenuid == null) alert("กรุณาเลือกเมนูอาหารก่อนการสั่งอาหาร");
+    this.httpClient.get("https://mrlunch.azurewebsites.net/api/Polls/VotePoll/" + this.model.id + "/" + this.selectmenuid)
       .subscribe((data: any) => {
-        this.model = data[0];
-        console.log(JSON.stringify(this.model));
+        if (data.isSuccess) {
+          this.navCtrl.push(ResualPage, this.model.id);
+        }
+        else alert(data.errorMessage);
       }, error => {
         alert(error.message);
       });
+  }
+
+  viewpoll(){
+    this.navCtrl.push(ResualPage, this.model.id);
+  }
+
+  doRefresh(refresher) {
+    setTimeout(() => {
+
+      this.httpClient.get("https://mrlunch.azurewebsites.net/api/Polls/GetRestaurantPolls")
+        .subscribe((data: any) => {
+          this.model = data[0];
+        }, error => {
+          alert(error.message);
+        });
       refresher.complete();
     }, 2000);
   }
